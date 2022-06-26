9920891e796c539883e78f7ef41d1a8f4cf98561
Beck <BeckJuraev@gmail.com>
2022-06-26 08:13:37 -0500

    Added Models [Course, Discount, Lead, Lesson]

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Course, Discount, Lesson, Lead
 
 # Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Discount)
+admin.site.register(Lesson)
+admin.site.register(Lead)
+
--- base/models.py
+++ base/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+
+class Discount(models.Model):
+    unit = models.IntegerField()
+    date_until = models.DateTimeField()
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    priority = models.IntegerField(default=1)
+
+
+class Lead(models.Model):
+    fio = models.CharField(max_length=200)
+    phone = models.IntegerField()
+    company = models.BooleanField(default=False)
+    company_name = models.CharField(max_length=200, blank=True, null=True)
+    inn = models.IntegerField(blank=True, null=True)
+
+
+
+
